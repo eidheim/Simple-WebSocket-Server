134ce605c60debb0716e9dae4ef4ec02aa02533a
eidheim <eidheim@gmail.com>
2014-08-23 09:54:15 +0200

    removed newlines from Base64::encode

--- crypto.hpp
+++ crypto.hpp
@@ -20,6 +20,7 @@ namespace SimpleWeb {
                 BUF_MEM *bptr;
 
                 b64 = BIO_new(BIO_f_base64());
+                BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
                 bio = BIO_new(BIO_s_mem());
                 BIO_push(b64, bio);
                 BIO_get_mem_ptr(b64, &bptr);
