7778d725327f7dcba8dfc71cf220dee16044728a
eidheim <eidheim@gmail.com>
2017-01-01 19:53:11 +0100

    Reverted change in ordering of endpoints in SocketServer::write_handshake

--- server_ws.hpp
+++ server_ws.hpp
@@ -482,9 +482,9 @@ namespace SimpleWeb {
         
         void write_handshake(const std::shared_ptr<Connection> &connection, const std::shared_ptr<boost::asio::streambuf> &read_buffer) {
             //Find path- and method-match, and generate response
-            for(auto endpoint_it=endpoint.rbegin();endpoint_it!=endpoint.rend();++endpoint_it) {
+            for(auto &endp: endpoint) {
                 REGEX_NS::smatch path_match;
-                if(REGEX_NS::regex_match(connection->path, path_match, endpoint_it->first)) {
+                if(REGEX_NS::regex_match(connection->path, path_match, endp.first)) {
                     auto write_buffer=std::make_shared<boost::asio::streambuf>();
                     std::ostream handshake(write_buffer.get());
 
@@ -492,14 +492,14 @@ namespace SimpleWeb {
                         connection->path_match=std::move(path_match);
                         //Capture write_buffer in lambda so it is not destroyed before async_write is finished
                         boost::asio::async_write(*connection->socket, *write_buffer, 
-                                [this, connection, write_buffer, read_buffer, endpoint_it]
+                                [this, connection, write_buffer, read_buffer, &endp]
                                 (const boost::system::error_code& ec, size_t /*bytes_transferred*/) {
                             if(!ec) {
-                                connection_open(connection, endpoint_it->second);
-                                read_message(connection, read_buffer, endpoint_it->second);
+                                connection_open(connection, endp.second);
+                                read_message(connection, read_buffer, endp.second);
                             }
                             else
-                                connection_error(connection, endpoint_it->second, ec);
+                                connection_error(connection, endp.second, ec);
                         });
                     }
                     return;
