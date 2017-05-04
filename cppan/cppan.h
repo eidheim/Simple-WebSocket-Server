40f0a0f1f5799a336623bc988bb55b5766535b11
Funk <Eugen.Funk@dlr.de>
2017-05-04 16:53:47 +0200

    compiling ws_example works using cppan. wow

---
+++ cppan/cppan.h
@@ -0,0 +1,33 @@
+// This file is in public domain.
+
+// C++ Archive Network (CPPAN)
+
+#pragma once
+
+/*******************************************************************************
+*
+* general
+*
+******************************************************************************/
+
+#ifndef CPPAN_BUILD
+#define CPPAN_BUILD
+#endif
+
+#ifndef CPPAN_CONFIG
+#define CPPAN_CONFIG ""
+#endif
+
+#ifndef CPPAN_EXPORT
+#define CPPAN_EXPORT
+#endif
+
+#ifndef CPPAN_PROLOG
+#define CPPAN_PROLOG
+#endif
+
+#ifndef CPPAN_EPILOG
+#define CPPAN_EPILOG
+#endif
+
+/******************************************************************************/
